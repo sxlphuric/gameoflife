37fa0e5db9e56c647c2f4a04409773818f71cf66
Effectivement <rnssaketo@proton.me>
2026-09-08 13:25:27 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Game of life - C++ implementation";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = inputs: {
+    packages = builtins.mapAttrs (system: pkgs: {
+      gameoflife = 
+      {lib,stdenv}:
+        stdenv.mkDerivation {
+        pname = "gameoflife";
+        version = "4.0.1";
+
+        
+        src = let
+          files = ./main.cpp;
+
+        in lib.fileset.toSource {
+          root = ./.;
+          fileset = lib.fileset.trace ./main.cpp;
+        };
+      };
+
+      
+
+      default = inputs.self.packages.${system}.gameoflife;
+    }) inputs.nixpkgs.legacyPackages;
+  };
+}
